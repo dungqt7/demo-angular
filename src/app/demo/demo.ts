9eb6c8ec3e4d142008942c447c5719fcba91bae0
dungqt7 <thienqt7@gmail.com>
2018-03-15 10:55:18 +0700

    demo-project

---
+++ src/app/demo/demo.ts
@@ -0,0 +1,7 @@
+export class Demo {
+  id: number;
+  name: string;
+}
+
+
+// tslint:disable-next-line:class-name
---
+++ src/app/mock-demo.ts
@@ -0,0 +1,20 @@
+import { Demo } from '../app/demo/demo';
+export const a: Demo[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+
+];
+export const a1: Demo[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
